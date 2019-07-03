487311d25fd4175b295d9c8657c32eb556178be6
OKali <o.kalinowski27@gmail.com>
2019-07-03 09:53:15 -0400

    Added psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Pick a theme for the trivia game
+    //Trivia based on a specific TV show?
+    //The Office Trivia?
+// Make up 5 multiple choice questions
+// Write instructions in html
+// Make timer that starts once the player clicks start button
+// Make the page display the next question once the timer is up 
+    //regardless of it the player submitted an answer 
+    //(Next page is shows correct answer to the question)
