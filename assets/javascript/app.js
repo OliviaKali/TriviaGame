8e3c560cc38d3af7cabb70ff5103dea7d5d93746
OKali <o.kalinowski27@gmail.com>
2019-07-06 11:41:36 -0400

    Added questions into js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,58 @@
+var directionsText = document.getElementById('directions-text');
+
+
+
 // Pick a theme for the trivia game
-    //Trivia based on a specific TV show?
-    //The Office Trivia?
+//Trivia based on a specific TV show?
+//The Office Trivia?
 // Make up 5 multiple choice questions
 // Write instructions in html
 // Make timer that starts once the player clicks start button
 // Make the page display the next question once the timer is up 
-    //regardless of it the player submitted an answer 
-    //(Next page is shows correct answer to the question)
+//make the page display the next page, once play has submitted their answer
+//regardless of it the player submitted an answer 
+//(Next page is shows correct answer to the question)
+
+// Add click function in js
+
+
+// var questions = [
+//     {
+//         question1: "What is the next lyric in the 'Mulan' song Reflection: Look at me/ I will never pass for a perfect bride/ Or a perfect daughter" 
+//         choiceA: "Can it be I'm not meant to play this role",
+//         choiceB: "Can it be I'm not meant to play this part",
+//         choiceC: "Can it be I'm not meant to be a bride",
+//         correct: "B"
+//     },
+    // {
+    //     question2: "",
+    //     choiceA: "",
+    //     choiceB: "",
+    //     choiceC: "",
+    //     correct: ""
+    // },
+    // {
+    //     question3: "",
+    //     choiceA: "",
+    //     choiceB: "",
+    //     choiceC: "",
+    //     correct: ""
+    // },
+    // {
+    //     question4: "",
+    //     choiceA: "",
+    //     choiceB: "",
+    //     choiceC: "",
+    //     correct: ""
+    // },
+    // {
+    //     question5: "",
+    //     choiceA: "",
+    //     choiceB: "",
+    //     choiceC: "",
+    //     correct: ""
+    // }
+// ];
+
+// var score = 0;
+
